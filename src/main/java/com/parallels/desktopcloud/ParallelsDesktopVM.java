0072d616bda4ce5045b5bcb598b8c493b0c91a9a
Vadim Groznov <vgroznov@parallels.com>
2015-10-02 16:40:36 +0300

    Habitual parameterized log with formatting

--- src/main/java/com/parallels/desktopcloud/ParallelsDesktopVM.java
+++ src/main/java/com/parallels/desktopcloud/ParallelsDesktopVM.java
@@ -31,14 +31,13 @@ import hudson.slaves.ComputerLauncher;
 import hudson.util.ListBoxModel;
 import java.lang.reflect.Field;
 import java.util.logging.Level;
-import java.util.logging.Logger;
 import jenkins.model.Jenkins;
 import org.kohsuke.stapler.DataBoundConstructor;
 
 
 public class ParallelsDesktopVM implements Describable<ParallelsDesktopVM>
 {
-	private static final Logger LOGGER = Logger.getLogger("ParallelsDesktopVM");
+	private static final ParallelsLogger LOGGER = ParallelsLogger.getLogger("PDVM");
 
 	public enum PostBuildBehaviors
 	{
@@ -78,7 +77,7 @@ public class ParallelsDesktopVM implements Describable<ParallelsDesktopVM>
 		}
 		catch(Exception ex)
 		{
-			LOGGER.log(Level.SEVERE, ex.toString());
+			LOGGER.log(Level.SEVERE, "Error: %s", ex);
 		}
 		if (this.postBuildBehavior == null)
 			this.postBuildBehavior = PostBuildBehaviors.Suspend;
@@ -149,7 +148,7 @@ public class ParallelsDesktopVM implements Describable<ParallelsDesktopVM>
 			prevVmState = VMStates.Suspended;
 		else
 		{
-			LOGGER.log(Level.SEVERE, "Unexpected VM '" + vmid + "' state: " + state);
+			LOGGER.log(Level.SEVERE, "Unexpected VM '%s' state: %s", vmid, state);
 			prevVmState = VMStates.Suspended;
 		}
 	}
@@ -196,7 +195,7 @@ public class ParallelsDesktopVM implements Describable<ParallelsDesktopVM>
 		}
 		catch (Exception ex)
 		{
-			LOGGER.log(Level.SEVERE, ex.toString());
+			LOGGER.log(Level.SEVERE, "Error: %s", ex);
 		}
 	}
 
